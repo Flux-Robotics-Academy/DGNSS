a9c4c68bac033d4f783ce549f4cb2f78c254fccf
darcyjoseph <64890014+DarcyWJ@users.noreply.github.com>
2022-12-20 13:25:29 +1030

    reset

--- src/roverSRC/roverFunction/roverFunction.ino
+++ src/roverSRC/roverFunction/roverFunction.ino
@@ -14,13 +14,16 @@ SFE_UBLOX_GNSS rover;
 
 //The ESP32 core has a built in base64 library but not every platform does
 //We'll use an external lib if necessary.
-#include "base64.h" //Built-in ESP32 library for encoding binary into base64 strings and the reverse operation.
+//#include "base64.h" //Built-in ESP32 library for encoding binary into base64 strings and the reverse operation.
 
 
 //Global variables
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 long lastReceivedRTCM_ms = 0; //5 RTCM messages take approximately ~300ms to arrive at 115200bps
 int maxTimeBeforeHangup_ms = 10000; //If we fail to get a complete RTCM frame after 10s, then disconnect from caster
+int fix = 10; //10 = unitialised
+int carrierSolution = 5; //unitialised
+bool diffCheck = false;
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 
@@ -29,6 +32,7 @@ void setup() {
   Serial.begin(115200);
   Serial.println(F("==========ROVER BASE=========="));
 
+
   Wire.begin(); //Start I2C
   if (rover.begin() == false){ //Connect to the Ublox module using Wire port
     Serial.println(F("u-blox GPS not detected at default I2C address. Please check wiring. Freezing."));
@@ -36,14 +40,16 @@ void setup() {
   }
   Serial.println(F("u-blox module connected"));
 
+  rover.factoryDefault(); delay(5000);
+
   //Configure Input & Output Ports
-  rover.setSerialRate(57600, COM_TYPE_UART2);
+  rover.setSerialRate(57600, COM_PORT_UART2);
   rover.setI2COutput(COM_TYPE_UBX); //Turn off NMEA noise
   rover.setPortInput(COM_PORT_I2C, COM_TYPE_UBX | COM_TYPE_NMEA | COM_TYPE_RTCM3); //Be sure RTCM3 input is enabled. UBX + RTCM3 is not a valid state.
   rover.setPortInput(COM_PORT_UART2, COM_TYPE_RTCM3); //Be sure RTCM3 input is allowed to be recieved through UART2
-
   rover.setNavigationFrequency(1); //Set output in Hz
   rover.setAutoPVT(true); //Tell the GNSS to "send" each solution
+
   
   //      SETUP ROVER FOR RTCM OVER NTRIP
   // Uncomment if NTRIP communication required
@@ -67,6 +73,16 @@ void setup() {
 
     //Send Long and Lat over Serial
     //S
+
+  fix = rover.getFixType();    
+  Serial.print("System Fix Type: ");
+  Serial.print(fix);
+  Serial.println(" "); 
+
+  carrierSolution = rover.getCarrierSolutionType();
+  Serial.print("Carrier Solution Type: ");
+  Serial.print(carrierSolution);
+  Serial.println(" "); 
 
 }
 
@@ -257,15 +273,17 @@ void loop() {
     //export data from arduino to the Zed-f9p - over I2C
 
     if (Serial1.available()){
-      beginRadio()
+      beginRadio();
       while (Serial1.available()) Serial1.read(); //Empty buffer of any newline chars
     }
     if (rover.getPVT()){
       sendGNSSData();
     }
-    delay(500);
 
-  void beginRadio(){
+    delay(500);
+} 
+
+void beginRadio(){
       long rtcmCount = 0;
 
       uint8_t rtcmData[512 * 4]; //Most incoming data is around 500 bytes but may be larger
@@ -293,40 +311,63 @@ void loop() {
           return;
         }
 
-        }
+        
+    
 
-        }
+  }
         
   void sendGNSSData(){
 
+    Serial.println("-----NEW TRANSMISSION------------------");
+
     long latitude = rover.getLatitude();
     //float latitudeADJ = latitude/10000000;
-    Serial.print(F("Lat: "));
+    Serial.print(F(" Lat: "));
     Serial.print(latitude);
+    Serial.println(" ");
 
     long longitude = rover.getLongitude();
     //float longitudeADJ = longitude/10000000;
     Serial.print(F(" Long: "));
     Serial.print(longitude);
     Serial.print(F(" (degrees * 10^-7)"));
+    Serial.println(" ");
 
     long altitude = rover.getAltitude();
     Serial.print(F(" Alt: "));
     Serial.print(altitude);
     Serial.print(F(" (mm)"));
+    Serial.println(" ");
 
-    long accuracy = rover.getPositionAccuracy();
+    long accuracy = rover.getPositionAccuracy(); //Indoor = ~10m; //Outdoor + Clear Sky = ~15mm
     Serial.print(F(" 3D Positional Accuracy: "));
     Serial.print(accuracy);
     Serial.println(F(" (mm)"));
 
+    //Should be 4 ideally - also maybe 3
     int fix = rover.getFixType();
-    Serial.print(F(" System Fix Type: "));
-    Serial.print(fix)
+    Serial.print(F("System Fix Type: "));
+    Serial.print(fix);
+    Serial.println(" ");
+
+    //This should enter 1 or 2 --> preferably 2
+    carrierSolution = rover.getCarrierSolutionType();
+    Serial.print("Carrier Solution Type: ");
+    Serial.print(carrierSolution);
+    Serial.println(" ");
+
+    diffCheck = rover.getDiffSoln();
+    if (diffCheck == true){
+      Serial.println("Differential Correction NOT Applied");
+    }
+    else{
+      Serial.println("Differential Correction Applied");
+    }
+
 
     Serial.println();
   }
-}
+
 
 
 
